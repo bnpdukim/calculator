c29dd8dc5a2d74e33cdc8965b0c8d2e53679e28f
sajacaros <sajacall82@gmail.com>
2015-03-04 18:15:51 +0900

    mysql 연동 완료

--- src/main/java/com/bnpinnovation/calculator/web/CalculatorController.java
+++ src/main/java/com/bnpinnovation/calculator/web/CalculatorController.java
@@ -3,6 +3,7 @@ package com.bnpinnovation.calculator.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,8 @@ public class CalculatorController {
 	private static final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 	
 	@Autowired
+	@Qualifier("summaryCalculator")
+//	@Qualifier("basicCalculator")
 	private CalculatorService calculator;
 	
 	@RequestMapping(value = "/plus", method = RequestMethod.POST, produces="application/json", consumes="application/json")
