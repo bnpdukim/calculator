56054ce9150e0c282c54053c4a29dd3046bdfcea
sajacaros <sajacall82@gmail.com>
2015-03-05 11:29:13 +0900

    test template 추가 불필요한 maven 설정 제거

---
+++ src/test/java/com/bnpinnovation/calculator/dao/OperationSummaryDaoTest.java
@@ -0,0 +1,18 @@
+package com.bnpinnovation.calculator.dao;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class OperationSummaryDaoTest {
+	private static final Logger logger = LoggerFactory.getLogger(OperationSummaryDaoTest.class);
+	@Test
+	public void test() {
+		assertThat(true, equalTo(true));
+	}
+
+}
